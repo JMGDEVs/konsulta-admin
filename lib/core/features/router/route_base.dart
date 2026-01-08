48a3a931d7648ea23066a2f2633361a546269327
Jan Miko A. Guevarra <140967296+Pinoccchio@users.noreply.github.com>
2026-01-09 00:31:29 +0800

    KSPRO-10 (7)

--- lib/core/features/router/route_base.dart
+++ lib/core/features/router/route_base.dart
@@ -4,6 +4,7 @@ import 'package:konsulta_admin/core/features/authentication/presentation/pages/l
 import 'package:konsulta_admin/core/features/home_controller/home_state_controller.dart';
 import 'package:konsulta_admin/core/features/onboarding_queue/presentation/pages/application_review_screen.dart';
 import 'package:konsulta_admin/core/features/onboarding_queue/presentation/pages/pending_applications_screen.dart';
+import 'package:konsulta_admin/core/features/onboarding_queue/presentation/pages/under_review_screen.dart';
 import 'package:konsulta_admin/core/features/onboarding_queue/presentation/pages/verified_applications_screen.dart';
 import 'package:konsulta_admin/core/features/router/route_paths.dart';
 
@@ -44,10 +45,21 @@ class RoutePages {
     ),
     GoRoute(
       name: '/application-pending',
-      path: RoutePaths.applicationReview,
+      path: RoutePaths.pendingApplications,
       pageBuilder: (context, state) => CustomTransitionPage(
         key: state.pageKey,
         child: const PendingApplicationsScreen(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return FadeTransition(opacity: animation, child: child);
+        },
+      ),
+    ),
+    GoRoute(
+      name: '/under-review',
+      path: RoutePaths.underReview,
+      pageBuilder: (context, state) => CustomTransitionPage(
+        key: state.pageKey,
+        child: const UnderReviewScreen(),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           return FadeTransition(opacity: animation, child: child);
         },
--- lib/core/features/router/route_paths.dart
+++ lib/core/features/router/route_paths.dart
@@ -3,5 +3,7 @@ class RoutePaths {
   static const String dashboard = '/dashboard';
   static const String login = '/';
   static const String applicationReview = '/application-review';
+  static const String pendingApplications = '/pending-applications';
+  static const String underReview = '/under-review';
   static const String verifiedApplications = '/verified-applications';
 }
